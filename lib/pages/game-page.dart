63f593a2bbaf8e9c3217d86041bf052a5c15a43a
HowdyHowe <amiruddinstr@gmail.com>
2025-09-07 20:32:13 +0700

    Adding button to change the box count.

--- lib/pages/game-page.dart
+++ lib/pages/game-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wordle_project/widgets/button.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
@@ -9,19 +10,100 @@ class GamePage extends StatefulWidget {
 
 class _GamePageState extends State<GamePage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  int boxCount = 42;
+
+  void changeBox(int box) {
+    setState(() {
+      boxCount = box;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () => Navigator.pop(context),
-          child: Container(
-            width: 150,
-            height: 50,
-            decoration: BoxDecoration(
-                color: Colors.purple, borderRadius: BorderRadius.circular(15)),
-            child: Center(child: Text("Kembali")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Button(
+                    icon: Icons.face_5_rounded,
+                    width: 60,
+                    height: 60,
+                    iconSize: 30,
+                    round: true,
+                    onPressed: () => changeBox(24)),
+                Button(
+                    icon: Icons.face_5_rounded,
+                    width: 60,
+                    height: 60,
+                    iconSize: 30,
+                    round: true,
+                    onPressed: () => changeBox(30)),
+                Button(
+                    icon: Icons.face_5_rounded,
+                    width: 60,
+                    height: 60,
+                    iconSize: 30,
+                    round: true,
+                    onPressed: () => changeBox(36)),
+                Button(
+                    icon: Icons.face_5_rounded,
+                    width: 60,
+                    height: 60,
+                    iconSize: 30,
+                    round: true,
+                    onPressed: () => changeBox(42)),
+              ],
+            ),
           ),
-        ),
+          Center(
+            child: Container(
+              width: boxCount * 10,
+              height: boxCount * 10,
+              margin: EdgeInsets.all(10),
+              child: GridView.builder(
+                  padding: EdgeInsets.all(0),
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 60,
+                    childAspectRatio: 1,
+                  ),
+                  itemCount: boxCount,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                          border:
+                              Border.all(width: 2, color: Color(0xFFd1d1d1)),
+                          borderRadius: BorderRadius.circular(10)),
+                    );
+                  }),
+            ),
+          ),
+          // Center(
+          //   child: GestureDetector(
+          //     onTap: () => Navigator.pop(context),
+          //     child: Container(
+          //       width: 150,
+          //       height: 50,
+          //       decoration: BoxDecoration(
+          //           color: Colors.purple,
+          //           borderRadius: BorderRadius.circular(15)),
+          //       child: Center(child: Text("Kembali")),
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
